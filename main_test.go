95b2dd0c40a15f050d1093070c033e4caafb3ce8
gamiken <gamiken@kappa-lab.com>
2025-01-31 12:57:22 +0900

    GenText

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ import (
 	"google.golang.org/genai"
 )
 
-func Test_QuickStart(t *testing.T) {
+func genClient(t *testing.T) *genai.Client {
 	require.NoError(t, godotenv.Load(".env"))
 	ctx := context.Background()
 	apiKey := os.Getenv("GEMINI_API_KEY")
@@ -24,6 +24,12 @@ func Test_QuickStart(t *testing.T) {
 		},
 	)
 	require.NoError(t, err)
+	return client
+}
+
+func Test_QuickStart(t *testing.T) {
+	ctx := context.Background()
+	client := genClient(t)
 
 	// Call the GenerateContent method
 	result, err := client.Models.GenerateContent(ctx, "gemini-2.0-flash-exp", genai.Text("Tell me about New York?"), nil)
@@ -33,4 +39,32 @@ func Test_QuickStart(t *testing.T) {
 	require.NoError(t, err)
 
 	fmt.Println(text)
+}
+
+func Test_GenText(t *testing.T) {
+	ctx := context.Background()
+	client := genClient(t)
+
+	result, err := client.Models.GenerateContent(ctx, "gemini-2.0-flash-exp", genai.Text("1年は何日？"), nil)
+	require.NoError(t, err)
+
+	text, err := result.Text()
+	require.NoError(t, err)
+
+	fmt.Println(text)
+}
+
+func Test_GenTextStream(t *testing.T) {
+	ctx := context.Background()
+	client := genClient(t)
+
+	stream := client.Models.GenerateContentStream(ctx, "gemini-2.0-flash-exp", genai.Text("1年は何日？"), nil)
+
+	for result, err := range stream {
+		if err != nil {
+			require.NoError(t, err)
+		}
+
+		fmt.Print(result.Candidates[0].Content.Parts[0].Text)
+	}
 }
